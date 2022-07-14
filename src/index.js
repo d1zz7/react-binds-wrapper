1c86037d918f5cde2e8a9ccfe94c12a817a8c0c6
d1zz7 <muhametov.tahir@yandex.ru>
2022-07-14 12:10:18 +0600

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export function _If(props) {
+  const { children } = props
+  return (
+    <div>
+      {React.Children.map(children, (element) => {
+        if (!React.isValidElement(element)) return
+        const source = element.props
+        if (source._if) {
+          return React.cloneElement(element, source)
+        }
+        if (!source._else && source._if !== false) {
+          console.log(source)
+          return React.cloneElement(element, source)
+        }
+        if (source.hasOwnProperty('_for')) {
+        }
+      })}
+    </div>
+  )
+}
+
+export function _For(props) {
+  const { data, children } = props
+  return (
+    <div>
+      {data.map((value) => {
+        return React.Children.map(children, (child) =>
+          React.cloneElement(child, value)
+        )
+      })}
+    </div>
+  )
+}
+
+export function _Switch(props) {
+  const { of, children } = props
+  return (
+    <div>
+      {React.Children.map(children, (element) => {
+        if (!React.isValidElement(element)) return
+        const source = element.props
+
+        if (of === source.case) {
+          return React.cloneElement(element, source)
+        }
+      })}
+    </div>
+  )
 }
